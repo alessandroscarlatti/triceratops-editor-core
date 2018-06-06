a03337b7763fc1f5f262e5385497f98d150c8df9
alessandroscarlatti <violanotesnoextras@gmail.com>
2018-06-06 00:17:15 -0500

    fix tests

--- test/internalController.test.js
+++ test/internalController.test.js
@@ -2,29 +2,58 @@ const EditorContext = require("../src/editor/core/EditorContext").default;
 const PutObjectCommand = require("../src/editor/core/commands/private/PutObjectCommand").default;
 const PutArrayCommand = require("../src/editor/core/commands/private/PutArrayCommand").default;
 const PutValueCommand = require("../src/editor/core/commands/private/PutValueCommand").default;
+const assert = require('assert');
 
-describe('internal controller can perform commands', () => {
-    let commands = [
-        PutObjectCommand,
-        PutArrayCommand,
-        PutValueCommand,
-    ];
+suite('Internal Controller', function () {
+    let editorContext;
 
-    commands.forEach((CommandClass) => {
-        it(`perform ${CommandClass.handles()}`, function() {
-            console.log("command", CommandClass);
-            const editorContext = new EditorContext();
+    setup(function () {
+        editorContext = new EditorContext();
+    });
 
-            let result = editorContext.internalController.perform(
-                new CommandClass(editorContext, {
-                    path: "/wherever",
-                })
-            );
+    test("PUT_OBJECT command creates a JSON instance controller", function () {
+        let command = new PutObjectCommand(editorContext, {
+            path: "/newObject",
+        });
 
-            console.assert(result === true);
-            let controller = editorContext.controllerLookupTable.getAt("/wherever");
-            console.assert(result != null);
-            console.assert(controller.path === "/wherever");
-        })
+        // perform the command
+        let result = editorContext.internalController.perform(command);
+        assert(result);
+
+        // now a controller should exist at that path
+        let controller = editorContext.controllerLookupTable.getAt("/newObject");
+        assert.ok(controller);
+        assert.equal(controller.path, "/newObject");
+    });
+
+    test("PUT_ARRAY command creates a JSON instance controller", function () {
+        let command = new PutArrayCommand(editorContext, {
+            path: "/newArray",
+        });
+
+        // perform the command
+        let result = editorContext.internalController.perform(command);
+        assert(result);
+
+        // now a controller should exist at that path
+        let controller = editorContext.controllerLookupTable.getAt("/newArray");
+        assert.ok(controller);
+        assert.equal(controller.path, "/newArray");
+    });
+
+    test("PUT_VALUE command creates a JSON instance controller", function () {
+        let command = new PutValueCommand(editorContext, {
+            path: "/newValue",
+        });
+
+        // perform the command
+        let result = editorContext.internalController.perform(command);
+        assert(result);
+
+        // now a controller should exist at that path
+        let controller = editorContext.controllerLookupTable.getAt("/newValue");
+        assert.ok(controller);
+        assert.equal(controller.path, "/newValue");
     });
 });
+
--- test/mocha.test.js
+++ test/mocha.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const stuff = require('./something').default;
 
 describe('Mocha', function() {
-    it("assert is working", function() {
+    it("mocha is working", function() {
         assert(1 === 1);
         console.log("stuff", stuff)
     })

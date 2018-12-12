612d955461977d26c9ddb70ed13c2f307545be9f
alessandroscarlatti <violanotesnoextras@gmail.com>
2018-12-11 22:45:47 -0600

    core getting some real functionality now!

--- src/editor/core/attempt3/TriceratopsInstance.js
+++ src/editor/core/attempt3/TriceratopsInstance.js
@@ -1,5 +1,28 @@
 const TriceratopsPath = require("./TriceratopsPath").default;
 
+/**
+ * this probably best accompanies a transition
+ * in nomenclature from "instance" describing the triceratops instance.
+ * this could be the triceratops instance.
+ * the javascript instance would be attached referenced in it.
+ * if the object is ever changed, this reference would of course be
+ * considered out of date, so it would have to be updated anyway.
+ * This means it is not a problem to "store the object piecemeal!"
+
+ * because of references we can actually use the instance
+ * on the triceratops instance to update values quickly.
+
+ * though, really, what's the point of keeping a backing instance?
+ * how often would I really request the current state of the object?
+ * but if I did, I would want it to be readily available, not
+ * reconstructed each time.
+ * and if it doesn't really come with a terrible performance
+ * penalty for updating VALUES, then that might be OK.
+ * but the values could have cached functions that utilize the
+ * parent instance, which would be necessary, though
+ * a dynamic lookup without another stack frame might be
+ * just as cheap, if not cheaper.
+ */
 class TriceratopsInstance {
     constructor() {
         this._jsInstance = null;
@@ -10,6 +33,27 @@ class TriceratopsInstance {
     _getNextId() {
         this._instanceIdCounter++;
         return "id" + this._instanceIdCounter;
+    }
+
+    _getNewInstanceIdForPath(trcPath) {
+        if (trcPath.accessors.length === 0) {
+            return "$";
+        }
+        else {
+            return this._getNextId();
+        }
+    }
+
+    getJsInstanceByPath(trcPath) {
+        return this.getInstanceByPath(trcPath).instance;
+    }
+
+    get jsInstance() {
+        return this._jsInstance;
+    }
+
+    getJsInstanceById(id) {
+        return this._instances[id].instance
     }
 
     getInstanceByPath(trcPath) {
@@ -29,17 +73,7 @@ class TriceratopsInstance {
     }
 
     exists(trcPath) {
-        let parentInstance = this._instances.$;
-
-        for (let accessor of trcPath.accessors) {
-            let childId = parentInstance.children[accessor];
-            if (childId == null) {
-                return false;
-            }
-            parentInstance = this._instances[childId];
-        }
-
-        return true;
+        return (this.getIdForPath(trcPath) != null);
     }
 
     getIdForPath(trcPath) {
@@ -54,7 +88,7 @@ class TriceratopsInstance {
             let childId = parentInstance.children[accessor];
 
             if (childId == null) {
-                throw new Error("Path does not exist: " + trcPath)
+                return null;  // path does not exist
             }
 
             parentInstance = this._instances[childId];
@@ -65,20 +99,10 @@ class TriceratopsInstance {
 
     createObjectNodeByPath(trcPath) {
 
-        let id;
-        if (trcPath.accessors.length === 0) {
-            id = "$";
-        }
-        else {
-            id = this._getNextId();
-        }
-
-        let parentId;
-        if (trcPath.accessors.length === 0) {
-            parentId = "$";
-        } else {
-            parentId = this.getIdForPath(trcPath.parent);
-        }
+        let id = this._getNewInstanceIdForPath(trcPath);
+        let parentId = this._getParentIdOrNull(trcPath);
+        let newJsInstance = {};
+        this._createChildInstance(parentId, trcPath.name, newJsInstance);
 
         this._instances[id] = {
             id: id,
@@ -86,28 +110,23 @@ class TriceratopsInstance {
             parent: parentId,
             nodeType: "OBJECT_NODE",
             meta: {},
-            children: {}
+            children: {},
+            instance: newJsInstance
         };
 
         // update the children map of the parent
-        this._instances[parentId].children[trcPath.name] = id;
+        if (parentId != null) {
+            this._instances[parentId].children[trcPath.name] = id;
+        }
+
+        return this._instances[id];
     }
 
     createArrayNodeByPath(trcPath) {
-        let id;
-        if (trcPath.accessors.length === 0) {
-            id = "$";
-        }
-        else {
-            id = this._getNextId();
-        }
-
-        let parentId;
-        if (trcPath.accessors.length === 0) {
-            parentId = "$";
-        } else {
-            parentId = this.getIdForPath(trcPath.parent);
-        }
+        let id = this._getNewInstanceIdForPath(trcPath);
+        let parentId = this._getParentIdOrNull(trcPath);
+        let newJsInstance = [];
+        this._createChildInstance(parentId, trcPath.name, newJsInstance);
 
         this._instances[id] = {
             id: id,
@@ -115,46 +134,83 @@ class TriceratopsInstance {
             parent: parentId,
             nodeType: "ARRAY_NODE",
             meta: {},
-            children: []
+            children: [],
+            instance: newJsInstance
         };
 
         // update the children map of the parent
-        this._instances[parentId].children[trcPath.name] = id;
+        if (parentId != null) {
+            this._instances[parentId].children[trcPath.name] = id;
+        }
+        return this._instances[id];
     }
 
     createValueNodeByPath(trcPath, value) {
         // does path exist?
         if (this.exists(trcPath)) {
-            this._updateValueNodeByPath(trcPath, value);
+            throw new Error("Value node already exists at path " + trcPath);
         } else {
-            this._createValueNodeByPath(trcPath, value);
+            return this._createValueNodeByPath(trcPath, value);
         }
     }
 
-    _updateValueNodeByPath(trcPath, value) {
-        // update the value
-
+    updateValueNodeByPath(trcPath, newValue) {
         let id = this.getIdForPath(trcPath);
-        this._instances[id].value = value;
+        return this.updateValueNodeById(id, newValue);
+    }
+
+    updateValueNodeById(id, newValue) {
+        let valueNode = this._instances[id];
+        valueNode.instance = newValue;
+        // also update the actual js instance
+        let parentId = valueNode.parent;
+        let parentJsInstance = this._instances[parentId].instance;
+        parentJsInstance[valueNode.accessor] = newValue;
+
+        return this._instances[id];
     }
 
     _createValueNodeByPath(trcPath, value) {
         // create a new value node
         // create a new id
         let id = this._getNextId();
-        let parentId = this.getIdForPath(trcPath.parent);
+        let parentId = this._getParentIdOrNull(trcPath);
+        let newJsInstance = value;
+        this._createChildInstance(parentId, trcPath.name, value);
 
         this._instances[id] = {
             id: id,
             accessor: trcPath.name,
             parent: parentId,
             nodeType: "VALUE_NODE",
-            value: value,
             meta: {},
+            instance: newJsInstance
         };
 
         // update the children map of the parent
         this._instances[parentId].children[trcPath.name] = id;
+        return this._instances[id];
+    }
+
+    _getParentIdOrNull(trcPath) {
+        if (trcPath.accessors.length === 0) {
+            return null;
+        } else {
+            return this.getIdForPath(trcPath.parent);
+        }
+    }
+
+    _createChildInstance(parentId, childAccessor, childInstance) {
+        // create the actual JS instance
+        // must first get the parent, then add the field.
+        if (parentId == null) {
+            // this is the root
+            this._jsInstance = childInstance;
+        } else {
+            // this is not the root
+            let parentJsInstance = this._instances[parentId].instance;
+            parentJsInstance[childAccessor] = childInstance;
+        }
     }
 }
 
--- src/editor/core/attempt3/TriceratopsInstance.test.js
+++ src/editor/core/attempt3/TriceratopsInstance.test.js
@@ -3,7 +3,7 @@ const TriceratopsPath = require("./TriceratopsPath").default;
 const assert = require("assert");
 
 describe("TriceratopsInstance", () => {
-    it ("can create an instance", () => {
+    it("can create an instance", () => {
         // create an instance
         let trc = new TriceratopsInstance();
 
@@ -15,12 +15,93 @@ describe("TriceratopsInstance", () => {
         trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][0]"), "Charlotte");
         trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][1]"), "Annie");
 
+        // update a value
+        trc.updateValueNodeByPath(TriceratopsPath.fromStr("$[friends][1]"), "Annie2");
+
         console.log(trc);
 
-        console.assert(trc.getInstanceByPath(TriceratopsPath.fromStr("$[name]")).value === "Phil");
+        console.assert(trc.getInstanceByPath(TriceratopsPath.fromStr("$[name]")).instance === "Phil");
         assert.equal(Object.keys(trc.getInstanceByPath(TriceratopsPath.fromStr("$[house]")).children).length, 1);
-        assert.equal(trc.getInstanceByPath(TriceratopsPath.fromStr("$[house][color]")).value, "red");
+        assert.equal(trc.getInstanceByPath(TriceratopsPath.fromStr("$[house][color]")).instance, "red");
         console.assert(trc.getInstanceByPath(TriceratopsPath.fromStr("$[friends][0]")) != null);
-        assert.equal(trc.getInstanceByPath(TriceratopsPath.fromStr("$[friends][0]")).value, "Charlotte")
+        assert.equal(trc.getInstanceByPath(TriceratopsPath.fromStr("$[friends][0]")).instance, "Charlotte");
+
+
+        console.assert(trc.getInstanceById("id0") != null);
+    });
+
+    it("can update a value", () => {
+        let trc = new TriceratopsInstance();
+
+        trc.createObjectNodeByPath(TriceratopsPath.fromStr("$"));
+        trc.createArrayNodeByPath(TriceratopsPath.fromStr("$[friends]"));
+        trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][0]"), "Charlotte");
+        trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][1]"), "Annie");
+
+        // update a value
+        trc.updateValueNodeByPath(TriceratopsPath.fromStr("$[friends][1]"), "Annie2");
+
+        assert.equal(trc.getInstanceByPath(TriceratopsPath.fromStr("$[friends][1]")).instance, "Annie2");
+    });
+
+    it("return newly created node", () => {
+        let trc = new TriceratopsInstance();
+
+        let rootNode = trc.createObjectNodeByPath(TriceratopsPath.fromStr("$"));
+        let friendsNode = trc.createArrayNodeByPath(TriceratopsPath.fromStr("$[friends]"));
+        let friends0Node = trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][0]"), "Charlotte");
+        let friends1Node = trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][1]"), "Annie");
+
+        // update a value
+        let updatedFriends1Node = trc.updateValueNodeByPath(TriceratopsPath.fromStr("$[friends][1]"), "Annie2");
+
+        assert(rootNode != null);
+        assert(friendsNode != null);
+        assert(friends0Node != null);
+        assert(friends1Node != null);
+        assert(updatedFriends1Node.id === friends1Node.id)
+    });
+
+    it ("access the js instance", () => {
+        let trc = new TriceratopsInstance();
+
+        let rootNode = trc.createObjectNodeByPath(TriceratopsPath.fromStr("$"));
+        let friendsNode = trc.createArrayNodeByPath(TriceratopsPath.fromStr("$[friends]"));
+        let friends0Node = trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][0]"), "Charlotte");
+        let friends1Node = trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][1]"), "Annie");
+
+        let instanceById = trc.getJsInstanceById("$");
+
+        assert.deepEqual(instanceById, {
+            friends: [
+                "Charlotte", "Annie"
+            ]
+        });
+        assert.deepEqual(trc.jsInstance, instanceById)
+
+        let instanceByPath = trc.getJsInstanceByPath(TriceratopsPath.fromStr("$"));
+        assert.deepEqual(instanceByPath, {
+            friends: [
+                "Charlotte", "Annie"
+            ]
+        });
+
+        let friendsArray = trc.getJsInstanceByPath(TriceratopsPath.fromStr("$[friends]"));
+        assert.deepEqual(friendsArray, ["Charlotte", "Annie"]);
+    });
+
+    it("update js instance", () => {
+        let trc = new TriceratopsInstance();
+
+        let rootNode = trc.createObjectNodeByPath(TriceratopsPath.fromStr("$"));
+        let friendsNode = trc.createArrayNodeByPath(TriceratopsPath.fromStr("$[friends]"));
+        let friends0Node = trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][0]"), "Charlotte");
+        let friends1Node = trc.createValueNodeByPath(TriceratopsPath.fromStr("$[friends][1]"), "Annie");
+
+        let updatedFriends1Node = trc.updateValueNodeById(friends1Node.id, "Annie2");
+
+        assert.equal(updatedFriends1Node.instance, "Annie2");
+        assert.equal(trc.jsInstance.friends[1], "Annie2");
+
     });
 });
